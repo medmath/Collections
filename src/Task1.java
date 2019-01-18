df618d41484dd409e09242e2ed6317fb6d064137
MedMath <medmathandit@gmail.com>
2019-01-18 13:28:47 -0500

    add Collections First Commit

---
+++ src/Task1.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter names:");
+        List<String> ls= new ArrayList<>();
+        for (int i = 0; i <5 ; i++) {
+        ls.add(sc.next());
+
+
+        }
+        System.out.println(ls.size());
+        System.out.println(ls);
+
+
+        Set<String> str= new HashSet<>(ls);
+        System.out.println(str.size());
+        System.out.println(str);
+
+
+
+
+
+        //String name1,name2,name3,name4,name5;
+//        List<String> ls= new ArrayList<>();
+//        while(sc.hasNext()){
+//            ls.add(sc.next());
+//            if(sc.next().equals("quit"))
+//                break;
+//        }
+//        System.out.println("original list: "+ls);
+//        System.out.println(ls.size());
+//
+//        Set<String> strSet= new HashSet<>(ls);
+//        System.out.println("new list: "+strSet);
+
+
+
+    }
+
+}
